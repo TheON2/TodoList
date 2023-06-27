1392a5fa36493865e57ca867a5daa5f1d67825e0
TheON2 <sniperad@naver.com>
2023-06-27 09:47:37 +0900

    Update : README

--- lv-4/src/api/todos.js
+++ lv-4/src/api/todos.js
@@ -1,24 +1,24 @@
 import axios from "axios";
 
 const addTodo = async (newTodo) => {
-  await axios.post(`${process.env.REACT_APP_LOCAL_SERVER}/todos`, newTodo);
+  await axios.post(`/todos`, newTodo);
 };
 
 const getTodos = async () => {
-  const response = await axios.get(`${process.env.REACT_APP_LOCAL_SERVER}/todos`);
+  const response = await axios.get(`/todos`);
   return response.data;
 };
 
 const updateDoneTodo = async (todo) => {
-  await axios.patch(`${process.env.REACT_APP_LOCAL_SERVER}/todos/${todo.id}`,{done:!(todo.done)});
+  await axios.patch(`/todos/${todo.id}`,{done:!(todo.done)});
 };
 
 const updateTodo = async (sendData) => {
-  await axios.patch(`${process.env.REACT_APP_LOCAL_SERVER}/todos/${sendData.id}`,{content:sendData.content});
+  await axios.patch(`/todos/${sendData.id}`,{content:sendData.content});
 };
 
 const deleteTodo = async (todoId) => {
-  await axios.delete(`${process.env.REACT_APP_LOCAL_SERVER}/todos/${todoId}`);
+  await axios.delete(`/todos/${todoId}`);
 };
 
 export { getTodos, addTodo , updateTodo , updateDoneTodo , deleteTodo };
